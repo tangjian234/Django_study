adc47b9666a274efda10304f4873b4adb1b8c74b
jian <tangjian234@gmail.com>
2019-12-16 15:35:28 -0800

    first commit

---
+++ DjangoLib.py
@@ -0,0 +1,20 @@
+
+from lxml import html
+import csv
+import os
+import requests
+from time import sleep
+from random import randint
+import re
+import requests
+import urllib
+import fnmatch
+
+#use of f string format
+def fPrint():
+    name = "Fred"
+    # a=23
+    s=f"He said his name is {a}."
+    print(s)
+
+    
